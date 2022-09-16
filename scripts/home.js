6c7973cc6fe3401bea85823c07eaa14ac90828e0
DuMaciel <edumaciel1221@gmail.com>
2022-09-16 18:04:33 -0300

    AddUnidades

--- scripts/home.js
+++ scripts/home.js
@@ -34,6 +34,18 @@ const inputs = [
     document.getElementById("diagonalMa"),
 ];
 
+const unity = [
+    document.getElementById("altura_s"),
+    document.getElementById("base_s"),
+    document.getElementById("alturaTra_s"),
+    document.getElementById("baseMe_s"),
+    document.getElementById("baseMa_s"),
+    document.getElementById("diagonalMe_s"),
+    document.getElementById("diagonalMa_s"),
+    document.getElementById("resultado_s"),
+];
+
+
 
 // Escutando
 for (let index = 0; index < buttons.length; index++) {
@@ -88,16 +100,16 @@ function resultado() {
     let retorno;
 
     if (buttons[0].classList.contains("clicado")) {
-        retorno = calTriangulo(parseFloat(inputs[0].value), parseFloat(inputs[1].value));
+        retorno = calTriangulo(parseFloat(inputs[0].value), unity[0].value, parseFloat(inputs[1].value), unity[1].value, unity[7].value);
 
     } else if (buttons[1].classList.contains("clicado")) {
-        retorno = calRetangulo(parseFloat(inputs[0].value), parseFloat(inputs[1].value));
+        retorno = calRetangulo(parseFloat(inputs[0].value), unity[0].value, parseFloat(inputs[1].value), unity[1].value, unity[7].value);
 
     } else if (buttons[2].classList.contains("clicado")) {
-        retorno = calTrapezio(parseFloat(inputs[2].value), parseFloat(inputs[3].value), parseFloat(inputs[4].value));
+        retorno = calTrapezio(parseFloat(inputs[2].value), unity[2].value, parseFloat(inputs[3].value), unity[3].value, parseFloat(inputs[4].value), unity[4].value, unity[7].value);
 
     } else if (buttons[3].classList.contains("clicado")) {
-        retorno = calLosango(parseFloat(inputs[5].value), parseFloat(inputs[6].value));
+        retorno = calLosango(parseFloat(inputs[5].value), unity[5].value, parseFloat(inputs[6].value), unity[6].value, unity[7].value);
     };
 
     document.getElementById("resultado").innerHTML = "Resultado: " + retorno;
@@ -110,22 +122,69 @@ function zerarInputs() {
     }
 }
 
-function calTriangulo(altura, base) {
-    const area = altura * base / 2;
+function calTriangulo(altura, alturaUni, base, baseUni, resultUni) {
+    altura = convertPadrao(altura, alturaUni);
+    base = convertPadrao(base, baseUni);
+
+    let area = altura * base / 2;
+    area = convertResult(area, resultUni);
     return area;
 }
 
-function calRetangulo(altura, base) {
-    const area = altura * base;
+function calRetangulo(altura, alturaUni, base, baseUni, resultUni) {
+    altura = convertPadrao(altura, alturaUni);
+    base = convertPadrao(base, baseUni);
+
+    let area = altura * base;
+    area = convertResult(area, resultUni);
     return area;
 }
 
-function calTrapezio(altura, baseMe, baseMa) {
-    const area = (baseMe + baseMa) * altura / 2;
+function calTrapezio(altura, alturaUni, baseMe, baseMeUni, baseMa, baseMaUni, resultUni) {
+    altura = convertPadrao(altura, alturaUni);
+    baseMe = convertPadrao(baseMe, baseMeUni);
+    baseMa = convertPadrao(baseMa, baseMaUni);
+
+    let area = (baseMe + baseMa) * altura / 2;
+    area = convertResult(area, resultUni);
     return area;
 }
 
-function calLosango(diagonalMe, diagonalMa) {
-    const area = diagonalMe * diagonalMa / 2;
+function calLosango(diagonalMe, diagonalMeUni, diagonalMa, diagonalMaUni, resultUni) {
+    diagonalMe = convertPadrao(diagonalMe, diagonalMeUni);
+    diagonalMa = convertPadrao(diagonalMa, diagonalMaUni);
+
+    let area = diagonalMe * diagonalMa / 2;
+    area = convertResult(area, resultUni);
     return area;
+}
+
+function convertPadrao(valor, unidade) {
+    if (unidade == 'mm') {
+        valor = valor / 1000;
+        return valor;
+    } else if (unidade == 'cm') {
+        valor = valor / 100;
+        return valor;
+    } else if (unidade == 'm') {
+        return valor;
+    } else if (unidade == 'km') {
+        valor = valor * 1000;
+        return valor;
+    }
+}
+
+function convertResult(valor, unidade) {
+    if (unidade == 'mm²') {
+        valor = valor * 1000000;
+        return valor;
+    } else if (unidade == 'cm²') {
+        valor = valor * 10000;
+        return valor;
+    } else if (unidade == 'm²') {
+        return valor;
+    } else if (unidade == 'km²') {
+        valor = valor / 1000000;
+        return valor;
+    }
 }
